7e07a4145cafaf37984cbea611f9531c342ce81e
Syndicate555 <saziz028@uottawa.ca>
2020-04-26 12:40:04 -0400

    Login and Registration complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,42 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Login from "./Components/Login";
 import Home from "./Components/pages/Home";
 import About from "./Components/pages/About";
 import Navbar from "./Components/layout/Navbar";
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
+import Register from "./Components/auth/Register";
+import Login from "./Components/auth/Login";
+import Alerts from "./Components/layout/Alerts";
+import setAuthToken from "./utils/setAuthToken";
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
 };
 
